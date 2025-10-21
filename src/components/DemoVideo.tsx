a261956fb22b3fce397f790bc577fc8c3511f9cf
David Tran <david_ff@ntucguest.com>
2025-10-21 14:41:54 +0700

    update video

--- src/components/DemoVideo.tsx
+++ src/components/DemoVideo.tsx
@@ -6,24 +6,23 @@ export default function DemoVideo() {
 
   return (
     <div
-      className="relative w-full aspect-[12/8] rounded-3xl overflow-hidden shadow-2xl bg-gradient-to-br from-[#B35832]/20 to-[#F5EFE6] cursor-pointer"
+      className="relative w-full aspect-[12/8] rounded-3xl overflow-hidden shadow-2xl cursor-pointer"
       onClick={() => setPlaying(true)}
     >
-      {playing ? (
-        <video
-          className="w-full h-full object-cover"
-          src="https://cdn.studyfoc.us/copy_88ECE7F6-3C56-465B-ACE1-0098B4C88124.mp4"
-          controls
-          autoPlay
-        />
-      ) : (
-        <div className="absolute inset-0 flex items-center justify-center bg-black/10">
-          <div className="text-center text-[#39241A]">
+      <video
+        className="w-full h-full object-cover"
+        src="https://cdn.studyfoc.us/copy_88ECE7F6-3C56-465B-ACE1-0098B4C88124.mp4"
+        controls
+        autoPlay
+      />
+      {!playing && (
+        <div className="absolute inset-0 flex items-center justify-center bg-gradient-to-br from-[#B35832]/20 to-[#F5EFE6] ">
+          <div className="text-center text-white">
             <IonIcon
               name="play-circle"
               className="text-6xl mx-auto mb-2"
             />
-            <p className="font-medium">Watch Demo Video</p>
+            <p className="font-medium text-white">Watch Demo Video</p>
           </div>
         </div>
       )}
